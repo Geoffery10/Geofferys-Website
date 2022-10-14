9615fa70d055516e02ee1cd6448cc5d053a7d5a2
Geoffery Powell <powellgeoffery@gmail.com>
2022-10-14 17:55:08 -0500

    Updated Old mcweb Links

---
+++ js/moddude.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $.getJSON('games.json', function(data) {
+        console.log(data.Games);
+        // Loop through the games in the json file and add them to a string (add "and" to the end)
+        var games = "";
+        for (var i = 0; i < data.Games.length; i++) {
+            if (data.Games.length > 2) {
+                if (i == data.Games.length - 1) {
+                    games += "and " + data.Games[i].Name;
+                } else {
+                    games += data.Games[i].Name + ", ";
+                }
+            } else {
+                if (i == data.Games.length - 1) {
+                    games += "and " + data.Games[i].Name;
+                } else {
+                    games += data.Games[i].Name + " ";
+                }
+            }
+
+        }
+
+        // Add the games to the html
+        $("#supported_games").html(games);
+
+        // Get version of moddude
+        $.getJSON(data.Game[0]['Mod URL'], function(data_02) {
+            console.log(data_02);
+            
+        });
+    });
+    
+});
