36e893dbe32280ab54cee2c8c8046c31594b39b0
Geoffery Powell <powellgeoffery@gmail.com>
2023-02-07 09:28:53 -0600

    Updated Mjolnir Content

--- js/moddude.js
+++ js/moddude.js
@@ -1,27 +1,27 @@
 $(document).ready(function(){
     $.getJSON('../games.json', function(data) {
         console.log(data.Games);
-        // Loop through the games in the json file and add them to a string (add "and" to the end)
-        var games = "";
+
+        // Loop through the games in the json file and add them to the supported_games div
         for (var i = 0; i < data.Games.length; i++) {
-            if (data.Games.length > 2) {
-                if (i == data.Games.length - 1) {
-                    games += "and " + data.Games[i].Name;
-                } else {
-                    games += data.Games[i].Name + ", ";
-                }
+            console.log(data.Games[i].Name);
+            if (data.Games[i].Name === "Minecraft") {
+                let img = document.createElement("img");
+                img.src = "/images/game_covers/Minecraft.png";
+                img.alt = data.Games[i].Name + " Cover";
+                document.getElementById("supported_games").appendChild(img);
+            } else if (data.Games[i].Name === "Bonelab") {
+                let img = document.createElement("img");
+                img.src = "/images/game_covers/Bonelab.png";
+                img.alt = data.Games[i].Name + " Cover";
+                document.getElementById("supported_games").appendChild(img);
             } else {
-                if (i == data.Games.length - 1) {
-                    games += "and " + data.Games[i].Name;
-                } else {
-                    games += data.Games[i].Name + " ";
-                }
+                let img = document.createElement("img");
+                img.src = "/images/game_covers/No_Cover.png";
+                img.alt = data.Games[i].Name + " Cover";
+                document.getElementById("supported_games").appendChild(img);
             }
-
         }
-
-        // Add the games to the html
-        $("#supported_games").html(games);
     });
 
     // Get version of moddude
